e8af561025a8079fe7cc1d984b071871bb389216
Digi-Mindset <gerhofer.pia@gmx.at>
2019-09-30 21:01:08 +0200

    Add FractionalTests and extend skeleton

---
+++ src/test/java/at/codersbay/datastructures/FractionalTest5.java
@@ -0,0 +1,14 @@
+package at.codersbay.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+
+public class FractionalTest3 {
+
+    @Test
+    public void testFractionalMultiplication() {
+    }
+
+}
