ccec8cfa6dec9ba8ca49608f89939cfec47fc253
mario-oberwalder <mario.oberwalder@gmail.com>
2019-10-10 14:34:49 +0200

    initial commit

--- src/main/java/at/codersbay/datastructures/Fractional.java
+++ src/main/java/at/codersbay/datastructures/Fractional.java
@@ -5,70 +5,137 @@ package at.codersbay.datastructures;
  * e.g.: 1/2, 1/3, 24/37, ..
  */
 public class Fractional {
+	
+	private Integer numerator;
+	private Integer denominator;
 
     public Fractional(Integer numerator, Integer denominator) {
-        // TODO implement to fix tests in FractionalTest1
+    	/*numerator can be 0 but not null*/
+    	if(numerator == null) {
+    		throw new IllegalArgumentException();
+    	} else {
+    		this.numerator = numerator;
+    	}
+    	
+    	/*denominator cant be 0 or null*/
+    	if(denominator == null || denominator == 0) {
+    		throw new IllegalArgumentException();
+    	} else {
+    		this.denominator = denominator;
+    	}
+        
+    }
+    
+    public Fractional invertedFractional(Fractional fractional) {
+    	Fractional returnFractional = new Fractional(
+    		fractional.getDenominator(), fractional.getNumerator());
+    	return returnFractional;	
     }
 
     public Integer getNumerator() {
-        // TODO implement to fix tests in FractionalTest1
-        return -1;
+        return this.numerator;
     }
 
     public Integer getDenominator() {
-        // TODO implement to fix tests in FractionalTest1
-        return -1;
+        return this.denominator;
     }
 
     public Float asFloat() {
-        // TODO implement to fix tests in FractionalTest2
-        return -1f;
+        return  ((float)this.numerator/(float) this.denominator);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        // TODO implement to fix tests in FractionalTest3
-        return false;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		Fractional fractionalObject;
+		Integer biggerDenom = 0;  
+		Integer smallerDenom = 0;
+		Integer biggerNum = 0; 
+		Integer smallerNum = 0;
+		/*!Obj is not a Fractional or is null*/
+		if (!obj.getClass().equals(this.getClass()) || obj == null) {
+			throw new IllegalArgumentException();
+		}
+		fractionalObject = (Fractional) obj;
+		if (fractionalObject.getDenominator() == 0
+				|| fractionalObject.getDenominator() == null
+				|| fractionalObject.getNumerator() == null) {
+			throw new IllegalArgumentException();	
+		}
+		/*Denom & Num are both equal*/
+		if (fractionalObject.getNumerator() == this.getNumerator()
+				&& fractionalObject.getDenominator() 
+				== this.getDenominator()) {
+			return true;
+		}
+		/*Numerator is 0*/
+		if(fractionalObject.getNumerator() == 0 
+				&& this.getNumerator() == 0) {
+			return true;
+		}
+		/*Numerators are equal after multiplication with other
+		 * fractionals denominator*/
+		if(this.getNumerator()*fractionalObject.getDenominator() 
+			== fractionalObject.getNumerator()*this.getDenominator()) 
+		{
+			return true;
+		}
+		
+
+		return false;
+	}
 
     public Fractional multiply(Fractional other) {
-        // TODO implement to fix tests in FractionalTest4
-        return new Fractional(1, 1);
+    	Integer calcDenom = 0;
+    	Integer calcNum = 0;
+    	calcDenom = this.getDenominator()*other.getDenominator();
+    	calcNum = this.getNumerator()*other.getNumerator();
+    	
+        return new Fractional(calcNum, calcDenom);
     }
 
     public Fractional multiply(Integer other) {
-        // TODO implement to fix tests in FractionalTest4
-        return new Fractional(1, 1);
+        return new Fractional((this.getNumerator()*other),(this.getDenominator()));
     }
 
     public Fractional divide(Fractional other) {
-        // TODO implement to fix tests in FractionalTest5
-        return new Fractional(1, 1);
+    	/*division by multiplication with the invers*/
+    	return this.multiply(this.invertedFractional(other));
     }
 
     public Fractional divide(Integer other) {
-        // TODO implement to fix tests in FractionalTest5
-        return new Fractional(1, 1);
+    	if (other == null || other == 0) {
+    		throw new IllegalArgumentException();
+    	} 
+    	return this.divide(new Fractional(other,1));
     }
 
     public Fractional add(Fractional other) {
-        // TODO implement to fix tests in FractionalTest6
-        return new Fractional(1, 1);
+    	int newDenom = 0;
+    	int newNum = 0;
+    	newNum = this.getNumerator()*other.getDenominator()+
+    			other.getNumerator()*this.getDenominator();
+    	newDenom = this.getDenominator()*other.getDenominator();
+        return new Fractional(newNum,newDenom);
     }
 
     public Fractional add(Integer other) {
-        // TODO implement to fix tests in FractionalTest6
-        return new Fractional(1, 1);
+    	if (other == null) {
+    		throw new IllegalArgumentException();
+    	}
+        return this.add(new Fractional(other, 1));
     }
 
     public Fractional subtract(Fractional other) {
-        // TODO implement to fix tests in FractionalTest7
-        return new Fractional(1, 1);
+     	int newDenom = 0;
+    	int newNum = 0;
+    	newNum = this.getNumerator()*other.getDenominator()-
+    			other.getNumerator()*this.getDenominator();
+    	newDenom = this.getDenominator()*other.getDenominator();
+        return new Fractional(newNum,newDenom);
     }
 
     public Fractional subtract(Integer other) {
-        // TODO implement to fix tests in FractionalTest7
-        return new Fractional(1, 1);
+        return this.subtract(new Fractional(other,1));
     }
 
 }
