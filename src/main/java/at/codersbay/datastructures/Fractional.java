a04ac54034c4a7b07cc1767d9cf4c088dcfc6100
mario-oberwalder <mario.oberwalder@gmail.com>
2019-10-16 09:31:50 +0200

    -refactoring especially equals(Object) -unified variables: factorialObject, newNum, newDenom

--- src/main/java/at/codersbay/datastructures/Fractional.java
+++ src/main/java/at/codersbay/datastructures/Fractional.java
@@ -10,26 +10,29 @@ public class Fractional {
 	private Integer denominator;
 
     public Fractional(Integer numerator, Integer denominator) {
-    	/*numerator can be 0 but not null*/
+    	/*numerator can be zero but not null*/
     	if(numerator == null) {
-    		throw new IllegalArgumentException();
+    		throw new IllegalArgumentException("You shall not pass null!");
     	} else {
     		this.numerator = numerator;
     	}
     	
-    	/*denominator cant be 0 or null*/
-    	if(denominator == null || denominator == 0) {
-    		throw new IllegalArgumentException();
-    	} else {
+    	/*denominator can not be zero or null*/
+    	if(denominator == null) {
+    		throw new IllegalArgumentException("You shall not pass null!");
+    	} else if (denominator == 0){
+    		throw new IllegalArgumentException("You shall not pass zero!");
+    	}
+    	else {
     		this.denominator = denominator;
     	}
         
     }
     
     public Fractional invertedFractional(Fractional fractional) {
-    	Fractional returnFractional = new Fractional(
+    	Fractional fractionalObject = new Fractional(
     		fractional.getDenominator(), fractional.getNumerator());
-    	return returnFractional;	
+    	return fractionalObject;	
     }
 
     public Integer getNumerator() {
@@ -46,55 +49,51 @@ public class Fractional {
 
 	@Override
 	public boolean equals(Object obj) {
-		Fractional fractionalObject;
-		/*!Obj is not a Fractional or is null*/
-		if (!obj.getClass().equals(this.getClass()) || obj == null) {
-			throw new IllegalArgumentException();
-		}
-		fractionalObject = (Fractional) obj;
-		if (fractionalObject.getDenominator() == 0
-				|| fractionalObject.getDenominator() == null
-				|| fractionalObject.getNumerator() == null) {
-			throw new IllegalArgumentException();	
-		}
-		/*Denom & Num are both equal*/
-		if (fractionalObject.getNumerator() == this.getNumerator()
-				&& fractionalObject.getDenominator() 
-				== this.getDenominator()) {
-			return true;
-		}
-		/*Numerator is 0*/
-		if(fractionalObject.getNumerator() == 0 
-				&& this.getNumerator() == 0) {
-			return true;
-		}
-		/*Numerators are equal after multiplication with other
-		 * fractionals denominator*/
-		if(this.getNumerator()*fractionalObject.getDenominator() 
-			== fractionalObject.getNumerator()*this.getDenominator()) 
-		{
-			return true;
-		}
-		
+		isValidFractional(obj); 
+		return numAndDenomMatch((Fractional) obj) || bothNumAreZero((Fractional) obj)
+				|| expandedNumsMatch((Fractional) obj);
+	}
 
-		return false;
+	private Boolean expandedNumsMatch(Fractional fractionalObject) {
+		return (this.getNumerator() * fractionalObject.getDenominator() == fractionalObject.getNumerator()
+				* this.getDenominator());
+
+	}
+
+	private Boolean bothNumAreZero(Fractional fractionalObject) {
+		return (fractionalObject.getNumerator() == 0 && this.getNumerator() == 0);
+
+	}
+
+	private Boolean numAndDenomMatch(Fractional fractionalObject) {
+		return (fractionalObject.getNumerator() == this.getNumerator()
+				&& fractionalObject.getDenominator() == this.getDenominator());
+	}
+
+	private void isValidFractional(Object obj) {
+		if (!obj.getClass().equals(this.getClass()) || obj == null) {
+			throw new IllegalArgumentException("Object is not a fractional or is null");
+		}
+		Fractional fractionalObject = (Fractional) obj;
+		if (fractionalObject.getDenominator() == 0 || fractionalObject.getDenominator() == null
+				|| fractionalObject.getNumerator() == null) {
+			throw new IllegalArgumentException("You shall not pass null!");
+		}
+
 	}
 
     public Fractional multiply(Fractional other) {
-    	Integer calcDenom = 0;
-    	Integer calcNum = 0;
-    	calcDenom = this.getDenominator()*other.getDenominator();
-    	calcNum = this.getNumerator()*other.getNumerator();
-    	
-        return new Fractional(calcNum, calcDenom);
+    	Integer newDenom = this.getDenominator()*other.getDenominator();;
+    	Integer newNum = this.getNumerator()*other.getNumerator();
+        return new Fractional(newNum, newDenom);
     }
 
     public Fractional multiply(Integer other) {
-        return new Fractional((this.getNumerator()*other),(this.getDenominator()));
+        return this.multiply(new Fractional(other,1));
     }
 
     public Fractional divide(Fractional other) {
-    	/*division by multiplication with the invers*/
+    	/*division by multiplication with the invers*/ 
     	return this.multiply(this.invertedFractional(other));
     }
 
@@ -106,11 +105,9 @@ public class Fractional {
     }
 
     public Fractional add(Fractional other) {
-    	int newDenom = 0;
-    	int newNum = 0;
-    	newNum = this.getNumerator()*other.getDenominator()+
+    	Integer newNum = this.getNumerator()*other.getDenominator()+
     			other.getNumerator()*this.getDenominator();
-    	newDenom = this.getDenominator()*other.getDenominator();
+    	Integer newDenom = this.getDenominator()*other.getDenominator();
         return new Fractional(newNum,newDenom);
     }
 
@@ -122,15 +119,16 @@ public class Fractional {
     }
 
     public Fractional subtract(Fractional other) {
-     	int newDenom = 0;
-    	int newNum = 0;
-    	newNum = this.getNumerator()*other.getDenominator()-
+    	Integer newNum = this.getNumerator()*other.getDenominator()-
     			other.getNumerator()*this.getDenominator();
-    	newDenom = this.getDenominator()*other.getDenominator();
+    	Integer newDenom = this.getDenominator()*other.getDenominator();
         return new Fractional(newNum,newDenom);
     }
 
     public Fractional subtract(Integer other) {
+    	if (other == null) {
+    		throw new IllegalArgumentException();
+    	}
         return this.subtract(new Fractional(other,1));
     }
 
